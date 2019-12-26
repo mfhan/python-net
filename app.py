c18416130c6c1d300682be6d3d2892c67e958c95
mfhan <mf212mf@gmail.com>
2019-12-26 17:00:28 -0500

    create layout file with code blocks, add bootstrap styling

--- app.py
+++ app.py
@@ -1,5 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'MF Han',
+        'title': 'Blog Intro',
+        'content': 'Let me introduce myself! I am learning Flask',
+        'date_posted': 'Dec 22, 2019'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Guest Blog Post',
+        'content': 'Yo, I am a guest poster',
+        'date_posted': 'Dec 26, 2019'
+    }
+]
 
 
 @app.route('/')
@@ -7,12 +22,15 @@ app = Flask(__name__)
 #     return '<h1>Hello, Blog Readers!</h1>'
 @app.route('/home')
 def home():
-    return '<h1>Hello, Blog Readers!</h1>'
+        # initially: return '<h1>Hello, Blog Readers!</h1>'
+    return render_template('home.html', posts=posts)
 
 
 @app.route('/about')
 def about():
-    return '<h1>About this repo</h1>'
+    # initially: return '<h1>About this repo</h1>'
+    # then: return render_template('about.html')
+    return render_template('about.html', title="About this project")
 
 
 if __name__ == '__main__':
